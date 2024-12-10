318fa738630559a460160ac125f6260afeb45664
Ksoucha <144827110+Ksoucha@users.noreply.github.com>
2024-12-10 18:15:30 -0500

    Create scripts for each character and create waypoint system

---
+++ Assets/Scripts/LutinBiscuit.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LutinBiscuit : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    public Transform waypoint1;
+    public Transform waypoint2;
+
+    Vector3 originalPosition;
+    public Animator animator;
+    private PlayerMovement player;
+
+    void Start()
+    {
+        originalPosition = transform.position;
+        StartCoroutine(BiscuitCoroutine());
+        player = FindAnyObjectByType<PlayerMovement>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    IEnumerator BiscuitCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+
+        animator.SetBool("IsWalking", true);
+        transform.forward = (waypoint1.position - transform.position).normalized;
+        while (Vector3.Distance(transform.position, waypoint1.position) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waypoint1.position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        animator.SetBool("IsWalking", false);
+
+        yield return new WaitForSeconds(2);
+
+        Vector3 direction = player.transform.position - transform.position;
+        direction.Normalize();
+        transform.forward = direction;
+
+        animator.SetTrigger("Dance");
+
+        yield return new WaitForSeconds(2);
+
+        animator.SetBool("IsWalking", true);
+        transform.forward = (waypoint2.position - transform.position).normalized;
+        while (Vector3.Distance(transform.position, waypoint2.position) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, waypoint2.position, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        animator.SetBool("IsWalking", false);
+
+        yield return new WaitForSeconds(2);
+
+        animator.SetBool("IsWalking", true);
+        transform.forward = (originalPosition - transform.position).normalized;
+        while (Vector3.Distance(transform.position, originalPosition) > 0.001f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, originalPosition, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        animator.SetBool("IsWalking", false);
+    }
+}
