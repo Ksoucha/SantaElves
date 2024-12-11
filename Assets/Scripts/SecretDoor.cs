03b2734f3b38a40c65a779505c8192ec7866995f
Ksoucha <144827110+Ksoucha@users.noreply.github.com>
2024-12-10 23:42:32 -0500

    Create scripts and holdPositionOffset
    Scripts for each interactable object for now

---
+++ Assets/Scripts/SecretDoor.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretDoor : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
