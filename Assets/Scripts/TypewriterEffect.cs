3e57ea57db571ab6e7a7729a074e0a1488d394a8
Ksoucha <144827110+Ksoucha@users.noreply.github.com>
2024-12-11 20:04:38 -0500

    Merge

---
+++ Assets/Scripts/TypewriterEffect.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TypewriterEffect : MonoBehaviour
+{
+
+    [SerializeField] private float typewriterSpeed = 50f;
+
+    public Coroutine Run(string textToType, TMP_Text textLabel)
+    {
+        return StartCoroutine(TypeText(textToType, textLabel));
+    
+    }
+
+    private IEnumerator TypeText(string textToType, TMP_Text textLabel)
+    {
+
+        textLabel.text = string.Empty;
+
+        float time = 0;
+        int charIndex = 0;
+
+        while (charIndex < textToType.Length)
+        {
+
+            time += Time.deltaTime * typewriterSpeed;
+            charIndex = Mathf.FloorToInt(time);
+            charIndex = Mathf.Clamp(charIndex, 0, textToType.Length);
+
+            textLabel.text = textToType.Substring(0, charIndex);
+
+            yield return null;
+        }
+
+        textLabel.text = textToType;
+
+    }
+
+
+
+}
