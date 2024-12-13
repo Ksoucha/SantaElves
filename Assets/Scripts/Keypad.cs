69365f46a90ade8103f5f7c461477564d207b93d
Noémie Prévost <noemie.a.pr@gmail.com>
2024-12-12 22:29:36 -0500

    Ajout des personnages + décors

---
+++ Assets/Scripts/Keypad.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+
+    [SerializeField] private Text Answer;
+
+    public void Number(int number)
+    {
+        Answer.text += number.ToString();
+    }
+}
